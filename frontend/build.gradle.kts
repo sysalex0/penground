c0bf8ca57cd97c5ae1a72a58a5f92c2ab40fdddf
Alex Shiu <sysalex0@gmail.com>
2023-10-02 01:11:26 +0800

    fix

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -8,11 +8,9 @@ node {
     version.set("18.18.0")
     npmVersion.set("")
     yarnVersion.set("")
-    npmInstallCommand.set("install")
     distBaseUrl.set("https://nodejs.org/dist")
     download.set(true)
     workDir.set(file("${project.projectDir}/.cache/nodejs"))
-    npmWorkDir.set(file("${project.projectDir}/.cache/npm"))
     yarnWorkDir.set(file("${project.projectDir}/.cache/yarn"))
     nodeProjectDir.set(file("${project.projectDir}"))
     nodeProxySettings.set(ProxySettings.SMART)
@@ -20,10 +18,10 @@ node {
 
 tasks {
     named("yarn_build") {
-        dependsOn("yarn_install", "yarn_openapi")
+        dependsOn("yarn_install", "yarn_openapi", "yarn_cache_clean")
     }
 
     named("yarn_test") {
-        dependsOn("yarn_install", "yarn_openapi")
+        dependsOn("yarn_install", "yarn_openapi", "yarn_cache_clean")
     }
 }
