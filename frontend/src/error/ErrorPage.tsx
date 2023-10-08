a70006050415ae378ab46d40f6700a5b3e1673d2
Alex Shiu <sysalex0@gmail.com>
2023-10-08 13:02:51 +0800

    - error page

---
+++ frontend/src/error/ErrorPage.tsx
@@ -0,0 +1,16 @@
+import { Box, Typography } from '@mui/material';
+import errorPenguinBall from '../image/penguinBall/transparant/error/00016-970974728.png';
+import './Error.scss';
+
+const ErrorPage = () => {
+  return (
+    <Box id="error-page" className="errorPage">
+      <Box>
+        <Typography variant="h1">Oops!</Typography>
+        <Typography variant="body1">Sorry, You are reaching a not exist page</Typography>
+        <img src={errorPenguinBall} className="errorImage" />
+      </Box>
+    </Box>
+  );
+};
+export default ErrorPage;
