eb3ee96645c7421ec428d895562742bf1440ac4c
Alex Shiu <sysalex0@gmail.com>
2023-10-29 22:03:38 +0800

    - optimize SPA filter

--- backend/src/main/kotlin/penguin/penground/frontend/SpaWebFilter.kt
+++ backend/src/main/kotlin/penguin/penground/frontend/SpaWebFilter.kt
@@ -5,11 +5,23 @@ import jakarta.servlet.ServletException
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.util.StringUtils
 import org.springframework.web.filter.OncePerRequestFilter
 import java.io.IOException
 
 @Component
+/**
+ * This class is for Single Web Application (SPA). If separate build frontend(FE) and backend(BE), this class is meaningless
+ * It is only applicable when the build bundling frontend and backend
+ */
 class SpaWebFilter : OncePerRequestFilter() {
+    companion object {
+        private const val BACKEND_URIS_PREFIX = "/api"
+        private val WHITELISTED_URIS = listOf(
+            "/actuator/health"
+        )
+    }
+
     @Throws(ServletException::class, IOException::class)
     override fun doFilterInternal(
         request: HttpServletRequest,
@@ -17,9 +29,18 @@ class SpaWebFilter : OncePerRequestFilter() {
         filterChain: FilterChain
     ) {
         val path = request.requestURI
-        // 1.not backend request, 2.not static resources or files with file extensions, 3.is frontend page
-        if (!path.startsWith("/api") && !path.contains(".") && path.matches("/(.*)".toRegex())) {
-            // ensure that the frontend application can handle client-side routing and render the appropriate view based on the requested path
+
+        val isBackendRequest: (String) -> Boolean = { uri: String -> uri.startsWith(BACKEND_URIS_PREFIX) }
+        val isStaticResourceWithFileExtensionRequest: (String) -> Boolean =
+            { uri -> StringUtils.getFilenameExtension(uri)?.isNotEmpty() ?: false }
+        val isWhitelistedRequest: (String) -> Boolean =
+            { uri -> WHITELISTED_URIS.any { whitelistedUri -> uri == whitelistedUri } }
+
+        // ensure that the frontend application can handle client-side routing and render the appropriate view based on the requested path
+        if (!isBackendRequest(path) &&
+            !isStaticResourceWithFileExtensionRequest(path) &&
+            !isWhitelistedRequest(path)
+        ) {
             request.getRequestDispatcher("/").forward(request, response)
             return
         }
